db9e5be510a9819d1df95b007cfd6a89dd749d0f
Matt Hannan <matthannan1@gmail.com>
2017-06-03 16:01:52 -0400

    wk5-ex1 something something

---
+++ wk5-ex1.py
@@ -0,0 +1,9 @@
+# Parse the CDP data (see link above) to obtain the following
+# information: hostname, ip, model, vendor, and device_type 
+# (device_type will be either 'router', 'switch', or 'unknown').
+
+
+
+
+file_object = open("https://github.com/ktbyers/pynet/learnpy_ecourse/class5/cdp_data.py")
+print file_object 
