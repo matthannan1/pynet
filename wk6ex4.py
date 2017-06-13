7668aff901b350808d0e1a357cb4f5b1f4b13a66
Matt Hannan <matthannan1@gmail.com>
2017-06-13 14:19:06 -0400

    wk6ex5 and fixed modules

--- wk6ex4.py
+++ wk6ex4.py
@@ -1,21 +1,8 @@
 # convert IP to binary
 
-def ip2binary(ip_address):
-    # Master Control
-    if len(input) != 2:
-        sys.exit("Invalid input. Run program again with proper data.")
-
-    # Pull IP from argv
-    ip_addr = input[1]
-
+def ip2binary(ip_addr):
     # Convert IP string to ipList
     ipList = ip_addr.split('.')
-
-    # Verify IP Address Length
-    if len(ipList) != 4:
-        sys.exit("Invalid IP address.")
-
-    # convert octets to Binary
     binList = []
     for i in ipList:
         binOct = bin(int(i))
--- wk6ex5.py
+++ wk6ex5.py
@@ -1,11 +1,15 @@
 import sys
 import wk6ex3a
 import wk6ex4
-string1 = "IP Address"
-string2 = "Binary"
 print
 
 input = raw_input('Enter an IP address: ')
-print wk6ex3a.IPvalidator(input)
-print "%-20s %-20s" % (string1, string2)
-print "%-20s %-20s" % (wk6ex4.ip_addr, binAddy)
+
+if wk6ex3a.IPvalidator(input):
+    string1 = "IP Address"
+    string2 = "Binary"
+#    print input
+    print "%-20s %-20s" % (string1, string2)
+    print "%-20s %-20s" % (input, wk6ex4.ip2binary(input))
+else:
+    print "Bad IP, yo"
