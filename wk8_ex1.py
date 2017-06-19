ae031f7fa5be0841785a7ffe192688758f7242df
Matt Hannan <matthannan1@gmail.com>
2017-06-19 14:32:37 -0400

    wk8_ex1 - IP validator module

---
+++ wk8_ex1.py
@@ -0,0 +1,92 @@
+# IP address validator with Try-Exception as Function
+
+def IPvalidator(ip_address):
+    ipList = ip_address.split('.')
+    validIP = True
+# Does input have four sections?
+    if len(ipList) != 4:
+        #print("Invalid input. Incorrect number of octets. Err 1")
+        validIP = False
+
+# Are the four sections integers?
+#    print list(enumerate(ipList))
+#    octets = []
+    for i, octet in enumerate(ipList):
+        try:
+            ipList[i] = int(octet)
+            #print(ipList[i])
+        except TypeError:
+            #print("Invalid input. %d is not an integer. Err 2" % ipList[i])
+            validIP = False
+        except ValueError:
+            #print("Invalid input. %s is not an integer. Err 2" % octet)
+            validIP = False
+
+# First octet must be 223 or less
+    try:
+        if int(ipList[0]) <= 0 or int(ipList[0]) > 223:
+            #print("Invalid input. %d is not a valid octet. Err 3" % ipList[0])
+            validIP = False
+    except ValueError:
+        #print("Invalid input. %s is not a valid octet. Err 3" % ipList[0])
+        validIP = False
+
+# First octet cannot be 127
+    try:
+        if int(ipList[0]) == 127:
+            #print("Invalid input. IP address can not be in 127.0.0.0 subnet. Err 4")
+            validIP = False
+    except ValueError:
+        #print("Invalid input. %s is not an integer. Err 4" % ipList[0])
+        validIP = False
+
+# IP cannot be on the 169.254.0.0 network
+    try:
+        if int(ipList[0]) == 169 and int(ipList[1]) == 254:
+            #print("Invalid input. IP address can not be in 169.254.0.0 subnet. Err 5")
+            validIP = False
+    except ValueError:
+        #print("Invalid input. %s is not an integer. Err 5" % ipList[1])
+        validIP = False
+
+# Are the octets integers between 0 and 255?
+    for i in ipList:
+        try:
+            #if int(i) >= 1:
+            if int(i) < 0 or int(i) > 255:
+                #print("Invalid input. %d is not a valid octet. Err 6" % int(i))
+                validIP = False
+        except ValueError:
+            validIP = False
+
+    return validIP
+
+# Separate executable code from importable code
+if __name__ == '__main__':
+    print
+        # Create a bunch of test cases
+    test_ip_addresses = {
+        '192.168.1'     :   False,
+        '10.1.1.'       :   False,
+        '10.1.1.x'      :   False,
+        '0.77.22.19'    :   False,
+        '-1.88.99.17'   :   False,
+        '241.17.17.9'   :   False,
+        '127.0.0.1'     :   False,
+        '169.254.1.9'   :   False,
+        '192.256.7.7'   :   False,
+        '192.168.-1.7'  :   False,
+        '10.1.1.256'    :   False,
+        '1.1.1.1'       :   True,
+        '223.255.255.255':  True,
+        '223.0.0.0'     :   True,
+        '10.200.255.1'  :   True,
+        '192.168.17.1'  :   True,
+    }
+
+    for ip_addr,expected_result in test_ip_addresses.items():
+        if IPvalidator(ip_addr):
+            print("%s is valid." % ip_addr)
+        else:
+            print("%s is NOT valid." % ip_addr)
+
